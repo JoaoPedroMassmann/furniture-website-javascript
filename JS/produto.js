2d1aa71e512ca98aef5aa1d47fd25db623453ed0
JoaoPedroMassmann <110121610+JoaoPedroMassmann@users.noreply.github.com>
2025-06-17 18:54:48 -0300

    adjust semantic tags, add variable font size, adjust footer elements, initiate construction of responsive product page

---
+++ JS/produto.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    
+})
+
+function renderProduct(product) {
+    const container = document.getElementById("product_container");
+
+    container.innerHTML = ``
+}
